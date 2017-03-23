9653ef24794f816cd9b505425129670130a740a3
leleuj <leleuj@gmail.com>
2017-03-23 20:04:51 +0100

    add SAML support and remove the /views prefix

--- src/main/java/org/pac4j/demo/dw/Pac4JDemoConfiguration.java
+++ src/main/java/org/pac4j/demo/dw/Pac4JDemoConfiguration.java
@@ -3,13 +3,12 @@ package org.pac4j.demo.dw;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.dropwizard.Configuration;
-import org.pac4j.demo.dw.factory.Pac4jFactory1;
 import org.pac4j.demo.dw.factory.Pac4jFactory2;
 
 public class Pac4JDemoConfiguration extends Configuration {
 
     @JsonProperty("pac4j")
-    Pac4jFactory1 pac4jFactory = new Pac4jFactory1();
-    //Pac4jFactory2 pac4jFactory = new Pac4jFactory2();
+    //Pac4jFactory1 pac4jFactory = new Pac4jFactory1();
+    Pac4jFactory2 pac4jFactory = new Pac4jFactory2();
 
 }
